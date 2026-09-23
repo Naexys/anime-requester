c6c6b31e52e91d9ec3540d40ceac1dfab6c06c5c
Naexys <71014703+Naexys@users.noreply.github.com>
2026-09-23 09:55:23 +0200

    feat / wip: adapted structure and drafted js script
    Created api text field and button
    created empty js file and got items by id

---
+++ JS/registration.js
@@ -0,0 +1,8 @@
+const inputAPI = document.getElementById("input_api");
+const button_go = document.getElementById("go");
+
+button_go.addEventListener('click', () => {
+    // check api key (not empty, ...)
+    // store in sessionStorage
+    // go to home.html
+});
