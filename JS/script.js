79591f7b4cefd26b1336292698a77866448c63cc
VIKTOR <victorangerrenault2007@gmail.com>
2026-09-23 16:31:55 +0200

    feat : start affCard

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,5 @@
 // Gestion d'API et de fetch par Thomas
 const {  fetchByName, fetchByID, fetchByRank, fetchAvailableGenres, searchBySingleGenre, searchByMultipleGenres  } = require("./api.js");
+const {affCard} = require("./showcard.js");
+
+affCard(await fetchByName("OverFlow"))  
---
+++ JS/showcard.js
@@ -0,0 +1,10 @@
+function affCard(animes) {
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    board.appendChild(card);
+    card.innerHTML = `${animes[data][0][title]}`
+}
+
+
+export {affCard};
